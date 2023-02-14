6b90ed9e0f8597797ff4d456afc9819d95df5d9c
Barca545 <jamarioneal@gmail.com>
2023-02-14 18:38:48 -0500

    Added match info class. Made players dict.

--- Matchmacking.py
+++ Matchmacking.py
@@ -2,7 +2,18 @@ import random
 import discord 
 import warnings
 import asyncio
+import secrets
+import string
 warnings.filterwarnings("ignore", category=RuntimeWarning) 
+
+def make_pwd():
+    alphabet = string.digits + string.ascii_letters
+    pwd_length = 13
+    pwd = ''
+    for i in range(pwd_length):
+        pwd += ''.join(secrets.choice(alphabet))
+    print(pwd)
+
 
 class Player:
     def __init__ (self, disc_name, disc_id, ign, rank, champ):
@@ -12,14 +23,39 @@ class Player:
         self.rank = rank 
         self.champ = champ
 
+class Match_info:
+    def __init__ (self, lobby_type, players:dict, ):
+        self.lobby_type = lobby_type
+        self.lobby_name = 
+        self.blue_1 = players
+        self.blue_2 = blue_2
+        self.red_1 = red_1
+        self.red_2 = red_2
+    def lobby_name():
+        print('Lobby Name: '+ self.lobby_creator + "'s Lobby" )
+    def lobby_creator(players):
+        lobby_creator = (random.choice(players)).disc_name
+        print(lobby_creator)
+    def elo_diff(mmr_delta):
+        elo_diff = mmr_delta
+        print(elo_diff)
+    def password():
+        print('RSS' + make_pwd)
+    
+
+    
+    'Elo Difference: ' + str())
+
+
+
 dummy_supp_1 = Player('Test1#303030', 221397446066962435, 'Test 1', 1000, 'Lulu',  )
 dummy_supp_2 = Player('Test2#303030',221397446066962435,'Test 3', 3000, 'Soraka')
 dummy_adc_1 = Player('Test3#303030',221397446066962435, 'Test 3', 4500, 'MF')
 
+Top_queue = {'Test1#303030': dummy_supp_1,'Test2#303030': dummy_supp_2, 'Test3#303030':dummy_adc_1}
+Mid_queue = {'Test1#303030': dummy_supp_1,'Test2#303030': dummy_supp_2}
 ADC_queue = {'Test3#303030': dummy_adc_1} #Remove dummy players
 Sup_queue = {'Test1#303030': dummy_supp_1,'Test2#303030': dummy_supp_2} #Remove dummy players
-Mid_queue = {'Test1#303030': dummy_supp_1,'Test2#303030': dummy_supp_2}
-Top_queue = {'Test1#303030': dummy_supp_1,'Test2#303030': dummy_supp_2, 'Test3#303030':dummy_adc_1}
 
 Queues = {
     'ADC_queue': ADC_queue,
@@ -29,7 +65,12 @@ Queues = {
     }                                            
 #@tasks.loop(minutes=0) #make 5 in final deploy
 async def choose_players(lane:str,mmr_band):
-    players = []
+    players = {
+        'Top_queue': {'Blue','Red'}, 
+        'Mid_queue': {'Blue','Red'},
+        'ADC_queue': {'Blue','Red'},
+        'Sup_queue': {'Blue','Red'}
+    }
     lane_queue = Queues[lane +'_queue']        
     def mmr_check(blue_laner,red_laner, mmr_band): 
         delta_mmr = abs(blue_laner.rank - red_laner.rank)
@@ -48,7 +89,7 @@ async def choose_players(lane:str,mmr_band):
 #            print ('elif 1')
             return True 
     blue_laner = random.choice(list(lane_queue.values()))
-    players.append(blue_laner)
+    players[lane +'_queue']['Blue']
     del lane_queue[blue_laner.disc_name]                       
     red_laner = random.choice(list(lane_queue.values()))          
 #    del lane_queue[red_laner.disc_name] # if this runs then the len(lane_queue) == 0 cant do the +1 thing in the while because it will cause the check_mmr function to run even when the list is empty
@@ -58,14 +99,14 @@ async def choose_players(lane:str,mmr_band):
     while len(lane_queue) >= 1 and len(players) < 2 : #condition may have to be tweeked
 #        print('loop test')
         if checked_mmr == True:
-#            print('red laner is still' + red_laner.disc_name) #delete when finished
-            players.append(red_laner)
+#           print('red laner is still' + red_laner.disc_name) #delete when finished
+            players[lane +'_queue']['Red'] = red_laner
             #await players
             return players
         elif checked_mmr == Player:
 #            print('looped thru elif1') #delete when finished
-            red_player_2 = checked_mmr
-            players.append(red_player_2)
+            red_laner_2 = checked_mmr
+            players[lane +'_queue']['Red'] = red_laner_2
             #await players
             return players
         else:
@@ -84,7 +125,7 @@ async def solo_match_notification(players:list, lane):
     mmr_delta = abs(blue_player.rank - red_player.rank)
     lobby_creator = random.choice(players).ign
     lobby_name = lobby_creator +"'s Lobby" + ' ' + str(random.randint(0,105))
-    password = 'RSS' + str(random.randint(0,10043)) 
+    
     match_info = (  
     'Lobby Name: '+ str(lobby_name) +'\n'+
     'Lobby type: ' + lane +'\n'+
